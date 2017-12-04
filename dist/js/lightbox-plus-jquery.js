a12ccd75b54f6c725392f2f2363395b4063e0e2e
Alessandro Maculotti <maculotti.a@gmail.com>
2017-12-04 18:00:03 +1100

    bugfix iOs disablesScroll options with a fixed container

--- dist/js/lightbox-plus-jquery.js
+++ dist/js/lightbox-plus-jquery.js
@@ -10500,19 +10500,20 @@ return jQuery;
       }
     }
 
-    // Position Lightbox
-    var top  = $window.scrollTop() + this.options.positionFromTop;
-    var left = $window.scrollLeft();
-    this.$lightbox.css({
-      top: top + 'px',
-      left: left + 'px'
-    }).fadeIn(this.options.fadeDuration);
-
-    // Disable scrolling of the page while open
     if (this.options.disableScrolling) {
       $('html').addClass('lb-disable-scrolling');
+      this.$lightbox.css({
+        display: 'block'
+      });
+    }else{
+      // Position Lightbox
+      var top  = $window.scrollTop() + this.options.positionFromTop;
+      var left = $window.scrollLeft();
+      this.$lightbox.css({
+        top: top + 'px',
+        left: left + 'px'
+      }).fadeIn(this.options.fadeDuration);
     }
-
     this.changeImage(imageNumber);
   };
 
